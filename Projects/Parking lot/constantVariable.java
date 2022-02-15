d5d578bc45b3b5e2ad659e6a13bd0c10dd4d0f9a
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2022-02-15 22:52:22 +0600

    Parking lot project

---
+++ Projects/Parking lot/constantVariable.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parkinglotproject;
+
+ class constantVariable
+{
+    int carSpace=100;
+    public int getCarSpace()
+    {
+        return carSpace;
+    }
+    public void setCarSpace()
+    {
+        
+    }
+    int totalCar;
+  //  int balance=0;
+}
